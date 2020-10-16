bca339607d69a05ab57b9383783a7193e1a86490
github username <31523060+elishevaelbaz@users.noreply.github.com>
2020-10-16 13:48:27 -0400

    practice problem with queues

--- 21-Stacks and Queues/Queue.js
+++ 21-Stacks and Queues/Queue.js
@@ -47,3 +47,43 @@ class Queue {
     return oldFirst.val;
   }
 }
+
+// Example of using a queue (similar to singly linked list)
+// function 20day trailing avg
+// takes a number and asks you to give 20 day average
+// function trailingAvg(num, pastVals){
+
+//   if (pastVals.size === 20){
+//     pastVals.dequeue() 
+//   }
+//   pastVals.enqueue(num)
+//   let total = 0;
+//   let currentNode = pastVals.first
+//   while (currentNode.next){
+//     total += currentNode.val
+//     currentNode = currentNode.next
+//   }
+//   return total / pastVals.size
+// }
+
+// without needing to traverse (num is new amount, pastVals is a queue of 20 values, and total is the total from the day before)
+function trailingAvg(num, pastVals, total){
+
+  if (pastVals.size === 20){
+    let firstVal = pastVals.dequeue() 
+    total -= firstVal;
+  }
+  pastVals.enqueue(num)
+  total += num;
+  return total / pastVals.size
+}
+
+
+// another problem is to keep a running average of all the days,
+// so you won't need a queue
+// just store the average from yesterday, and the total number of days.
+
+// multiply the average by # of days
+// add the val from today
+// increemnt the # of days by 1
+// divide the total by new # of days
